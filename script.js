1530e695145602b1ceb5baddbe19f931a1f015e2
singhdevendrajtz <singhdevendrajtz@gmail.com>
2025-09-28 17:19:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const boxes=document.querySelectorAll(".code")
+boxes.forEach((x,y)=>{
+	boxes[y].addEventListener('keyup',function(){
+		if(boxes[y].length!=0 && y!=boxes.length-1)
+		{
+			boxes[y].nextElementSibling.focus()
+		}
+	})
+});
+boxes.forEach((x,y)=>{
+	boxes[y].addEventListener('keyup',function(){
+		let KeyID = event.keyCode;
+		 if(KeyID==8 && y!=0)
+		 {
+		 	boxes[y].previousElementSibling.focus()
+		 }
+		console.log(KeyID)
+			
+	})
+});
